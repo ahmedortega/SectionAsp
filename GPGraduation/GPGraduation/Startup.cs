a6723111ae11ef96b9eba8a646891c78b672e0c6
Eng'Mahmoud Rezk <messiman30@yahoo.com>
2018-04-01 15:00:03 +0200

    Finish

---
+++ GPGraduation/GPGraduation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GPGraduation.Startup))]
+namespace GPGraduation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
